372825a6b99766af06e68eea024c49b30bd82f67
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 17:46:51 +0200

    UPDATE: big update which supports posting and updating annotation, added new transport layers

--- SemantifyIt.php
+++ SemantifyIt.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace STI\SemantifyIt;
 
 use Exception;
@@ -7,17 +8,20 @@ use Exception;
 /**
  * Class SemantifyIt
  */
-class SemantifyIt {
+class SemantifyIt
+{
 
     /**
      * variable for websiteApiKey
-     *  @param string $websiteApiKey;
+     *
+     * @param string $websiteApiKey ;
      */
     private $websiteApiKey;
 
     /**
      * variable for Url
-     *  @param string $websiteKey;
+     *
+     * @param string $websiteKey ;
      */
     private $live_server = "https://semantify.it/api";
 
@@ -81,11 +85,12 @@ class SemantifyIt {
 
     /**
      * SemantifyIt constructor.
+     *
      * @param string $key
      */
     public function __construct($key = "")
     {
-        if($key!=""){
+        if ($key != "") {
             $this->setWebsiteApiKey($key);
         }
     }
@@ -99,26 +104,78 @@ class SemantifyIt {
      * @return string return content
      * @throws Exception
      */
-    private function get($url){
+    private function get($url)
+    {
 
         $content = @file_get_contents($url);
 
-        if($content===false){
-            throw new Exception('Error getting content from '.$url);
+        if ($content === false) {
+            throw new Exception('Error getting content from ' . $url);
         }
 
-        if($content==""){
-            throw new Exception('No content received from '.$url);
+        if ($content == "") {
+            throw new Exception('No content received from ' . $url);
         }
 
         return $content;
 
     }
 
-    private function isContentAvailable($input){
-        if(($input == "") || ($input == false) || (strpos($input, 'error') !== false)){
+    private function post($url, $params)
+    {
+
+        $content = $this->curl("POST", $url, $params);
+
+        if ($content === false) {
+            throw new Exception('Error posting content to ' . $url);
+        }
+
+        if ($content == "") {
+            throw new Exception('No content returned from POST action at url ' . $url);
+        }
+
+        return $content;
+
+    }
+
+
+    private function curl($type, $url, $params)
+    {
+
+        //var_dump($url);
+
+        $params_string = json_encode($params);
+
+        //var_dump($params_string);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,
+                    array('Content-Type: application/json', 'Content-Length: ' . strlen($params_string)));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+       // echo($response);
+
+        if (curl_error($ch) && ($this->error)) {
+            echo 'CURL error:' . curl_error($ch);
+        }
+
+        curl_close($ch);
+
+        return $response;
+
+    }
+
+    private function isContentAvailable($input)
+    {
+        if (($input == "") || ($input == false) || (strpos($input, 'error') !== false)) {
             return false;
         }
+
         return true;
     }
 
@@ -126,36 +183,55 @@ class SemantifyIt {
      *
      * transport layer for api
      *
-     * @param $type
-     * @param path
+     * @param       $type
+     * @param       path
      * @param array $params
      * @return string
      */
-    private function transport($type, $path, $params=array()){
+    private function transport($type, $path, $params = array())
+    {
 
         /** url with server and path */
-        $url = $this->live_server.'/'.$path;
+        $url = $this->live_server . '/' . $path;
         //if it is in staging server than switch to staging api
-        if($this->live==false){
-            $url = $this->staging_server.'/'.$path;
+        if ($this->live == false) {
+            $url = $this->staging_server . '/' . $path;
         }
 
         switch ($type) {
 
             case "GET":
-                try{
-                    $fullurl = $url.( count($params)==0 ? '' : '?'. http_build_query($params) );
+                try {
+                    $fullurl = $url . (count($params) == 0 ? '' : '?' . http_build_query($params));
+
                     return $this->get($fullurl);
+                } catch (Exception $e) {
+                    if ($this->error) {
+                        echo 'Caught exception: ' . $e->getMessage() . "<br>";
+                    }
+
+                    return false;
                 }
-                catch (Exception $e) {
-                    if($this->error){
-                        echo 'Caught exception: '.$e->getMessage(). "<br>";
+                break;
+
+            case "POST":
+            case "PATCH":
+                try {
+                    $fullurl = $url;
+
+                    //call the class method
+                    return call_user_func_array(array($this, strtolower($type)), array($fullurl, $params));
+
+                } catch (Exception $e) {
+                    if ($this->error) {
+                        echo 'Caught exception: ' . $e->getMessage() . "<br>";
                     }
 
                     return false;
                 }
 
-            break;
+                break;
+
 
         }
     }
@@ -167,13 +243,15 @@ class SemantifyIt {
      * @param $json
      * @return mixed
      */
-    private function decoding($json){
+    private function decoding($json)
+    {
         return json_decode($json);
     }
 
 
     /**
      * getter for websiteApiKey
+     *
      * @return string
      */
     public function getWebsiteApiKey()
@@ -184,6 +262,7 @@ class SemantifyIt {
 
     /**
      * setter for websiteApiKey
+     *
      * @param string $websiteApiKey
      */
     public function setWebsiteApiKey($websiteApiKey)
@@ -196,17 +275,53 @@ class SemantifyIt {
      *
      * @return array
      */
-    public function getAnnotationList(){
+    public function getAnnotationList()
+    {
 
         //$params["key"] = $this->getWebsiteApiKey();
         //$json = $this->transport("GET", "annotation/list/",$params);
 
         //if(!$this->isContentAvailable($json)) {
-            $json = $this->transport("GET", "annotation/list/".$this->getWebsiteApiKey());
+        $json = $this->transport("GET", "annotation/list/" . $this->getWebsiteApiKey());
+
         //}
 
         return $json;
     }
+
+    /**
+     * post a new annotation to the server
+     *
+     * @return array
+     */
+    public function postAnnotation($json)
+    {
+
+        $params["content"] = $json;
+        $json = $this->transport("POST", "annotation/" . $this->getWebsiteApiKey(), $params);
+
+
+        return $json;
+    }
+
+    /**
+     *
+     * update an annotation by uid
+     *
+     * @param $json
+     * @param $uid
+     * @return string
+     */
+    public function updateAnnotation($json, $uid)
+    {
+
+        $params["content"] = $json;
+        $json = $this->transport("PATCH", "annotation/".$uid."/" . $this->getWebsiteApiKey(), $params);
+
+
+        return $json;
+    }
+
 
     /**
      *
@@ -215,8 +330,9 @@ class SemantifyIt {
      * @param $url
      * @return string
      */
-    public function getAnnotationByURL($url){
-        return $this->transport("GET", "annotation/url/".rawurlencode($url));
+    public function getAnnotationByURL($url)
+    {
+        return $this->transport("GET", "annotation/url/" . rawurlencode($url));
     }
 
     /**
@@ -226,13 +342,12 @@ class SemantifyIt {
      * @param string $id
      * @return json
      */
-    public function getAnnotation($id){
+    public function getAnnotation($id)
+    {
 
-        return $this->transport("GET", "annotation/short/".$id);
+        return $this->transport("GET", "annotation/short/" . $id);
 
     }
-
-
 
 
 }
